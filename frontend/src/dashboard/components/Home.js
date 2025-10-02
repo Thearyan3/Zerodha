347d70ed86663cca2b9f7804efc01dcfecf445f5
Aryan <chaudharyaryan3456@gmail.com>
2025-10-03 01:30:04 +0530

    merging dashboard into frontend for easy deployment, currently not possible if both apps are seperate

--- frontend/src/dashboard/components/Home.js
+++ frontend/src/dashboard/components/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
+import '../dashboard.css';
 
 const Home = () => {
   return (
-    <>
+      <div className="dashboard-root">
       <TopBar />
       <Dashboard />
-    </>
+    </div>
   );
 };
 
